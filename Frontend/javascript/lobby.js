90a9649ff3a3c4c8e36dafba9c3ef6e20bb18a2d
Misbah-Rizaee <47888688+Misbah-Rizaee@users.noreply.github.com>
2021-10-25 10:48:35 +0100

    Timer and score done

--- Frontend/javascript/lobby.js
+++ Frontend/javascript/lobby.js
@@ -1,3 +1,9 @@
+var timer;
+var timedEvent;
+var playerName;
+var rectangle;
+var p1Score = 0;
+
 const lobbyScreen = new Phaser.Class({
     Extends: Phaser.Scene,
     initialize: function() {
@@ -5,24 +11,59 @@ const lobbyScreen = new Phaser.Class({
     },
 
     preload: function() {
-        // this.load.image('bg', 'assets/bg.png');
 
     },
 
     create: function() {
-        // let bg = this.add.image(0, 0, 'bg');
-        //
-        // bg.displayHeight=game.config.height;
-        // bg.scaleX=bg.scaleY;
-        //
-        // bg.y=game.config.height/2;
-        // bg.x=game.config.width/2;
+        rectangle = this.add.rectangle(0, 0, game.config.width*.3, game.config.height*.3, 0xFFFFFF).setInteractive();
 
-        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-        const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
-        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        const startGame = this.add.text(screenCenterX, screenCenterY, 'Start The game', style).setOrigin(0.5);
+        this.physics.add.existing(rectangle);
+
+        rectangle.body.velocity.x = 100;
+        rectangle.body.velocity.y = 100;
+        rectangle.body.bounce.x = 1;
+        rectangle.body.bounce.y = 1;
+        rectangle.body.collideWorldBounds = true;
+
+        //THROW
+        rectangle.on('pointerdown', function (pointer) {
+            // this.setTint(0xff0000);
+            console.log('pointerover');
+            p1Score += 1;
+            playerName.setText(username + ": " + p1Score);
+        });
+
+        // TIME IN SECOND
+        this.initialTime = 5;
+        timer = this.add.text(5, 5, 'Countdown: ' + formatTime(this.initialTime));
+        timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
+
+        //PLAYERS NAME
+        playerName = this.add.text(5, 25, username + ": " + p1Score);
+
     },
 
     update: function() {}
 });
+
+var username = prompt('Enter your name:');
+
+
+//TIMER FORMAT
+function formatTime(seconds)
+{
+    var minutes = Math.floor(seconds/60);
+    var partInSeconds = seconds%60;
+    partInSeconds = partInSeconds.toString().padStart(2,'0');
+    return `${minutes}:${partInSeconds}`;
+}
+
+//TIMER COUNTDOWN
+function onEvent ()
+{
+    this.initialTime -= 1; // One second
+    timer.setText('Countdown: ' + formatTime(this.initialTime));
+    if(this.initialTime==0){
+        alert(username+" Won the game");
+    }
+}
