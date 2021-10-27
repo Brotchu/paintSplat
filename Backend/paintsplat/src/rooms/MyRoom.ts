25730976cbfca292de3728d7304117a4533bf966
Mahislami <mahdislami1377@gmail.com>
2021-10-27 16:14:05 +0100

    Adding a synchronous timer to be shown on each client end

--- Backend/paintsplat/src/rooms/MyRoom.ts
+++ Backend/paintsplat/src/rooms/MyRoom.ts
@@ -1,11 +1,11 @@
-import { Room, Client } from "colyseus";
+import { Room, Client, Delayed } from "colyseus";
 import { MyRoomState } from "./schema/MyRoomState";
 import { Schema, MapSchema, type } from "@colyseus/schema";
 
 var colorCodes:string[]; 
 colorCodes = ["#191970","#006400","#ff0000","#ffd700","#00ff00","#00ffff","#ff00ff","#ffb6c1"]
 export class MyRoom extends Room<MyRoomState> {
-
+public delayedInterval!: Delayed;
   
   onCreate (options: any) {
     // console.log("room created")
@@ -13,23 +13,10 @@ export class MyRoom extends Room<MyRoomState> {
     // this.state.canvas.push("1")
     // this.state.canvas.push("2")
 
-    this.state.canvas.set ( "10,10", "Client_1" );
+    //this.state.canvas.set ( "10,10", "Client_1" );
 
-    this.onMessage("move", (client, message) => {
-      //console.log("received message")
-      //console.log(message)
-      //console.log(this)
-      //
-      // handle "type" message
-      //
-    });
-
-    this.onMessage("shoot", (client, message) => {
-      //console.log("player", client.sessionId, " took a shot", message)
-    });
 
     this.onMessage("shot", (client, message) => {
-      //console.log("player", client.sessionId, " took a shot", message)
       if(this.state.isRunning == true){
         var key = message.x + "," + message.y;
         this.state.canvas.set ( key, client.sessionId );
@@ -45,10 +32,16 @@ export class MyRoom extends Room<MyRoomState> {
         this.state.isRunning = true;
         this.broadcast("game_started", {state: this.state.canvas});
         this.clock.start();
-        this.clock.setTimeout(() => { console.log("Time now " + this.clock.currentTime);
+        this.delayedInterval = this.clock.setInterval(() => {
+          console.log("Time now " + this.clock.elapsedTime);
+          this.broadcast("Game Timer", ((this.clock.elapsedTime/1000).toFixed(0)));
+      }, 1000);
+        
+        this.clock.setTimeout(() => { console.log("Time now " + this.clock.elapsedTime);
         this.broadcast("Game is over",this.state.canvas);
         this.state.isRunning = false;
-        this.clock.stop();
+        this.delayedInterval.clear();
+        //this.clock.stop();
       }, 15_000);
       }
     });
